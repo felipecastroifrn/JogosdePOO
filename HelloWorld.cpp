694bec2bd19ff6efa34a2b42ef0abe3e08a009fa
Felipe Castro <felipecastro12345fc6@gmail.com>
2016-06-08 19:30:55 -0300

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
+#include <iostream>  //Bliblioteca
 using namespace std;
-
+                     //função principal
 int main() {
 	
-	cout << "HELLO  WORLD" << endl;
+	cout << "HELLO  WORLD" << endl; //imprimindo na tela "olá mundo"
 
 	
 	return 0;
-}
+}                   //término do programa
+  
